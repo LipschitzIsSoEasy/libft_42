261e56a7f1fe45092bbd6088438a5584817abc61
MeilingTIAN <meiling@MeilingTIANsAir.lan>
2024-05-29 01:10:03 +0200

    5.29 1

---
+++ ft_putendl_fd.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+void ft_putchar_fd(char c, int fd)
+{
+	write (fd, &c, 1);
+}
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+	{
+		return ;
+	}
+	while (s[i] != '\0')
+	{
+		ft_putchar_fd(s[i], fd);
+		i++;
+	}
+	ft_putchar_fd('\n', fd);
+}
---
+++ ft_split.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+
+int	count_words(char const *s, char c)
+{
+	
+}
+
+char **ft_split(char const *s, char c)
+{
+
+}
